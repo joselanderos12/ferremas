8c8260eaed50aa0dbc7ef3c4cbbbc444c1042b67
joselanderos12 <j.landeros@duocuc.cl>
2025-06-26 00:14:38 -0400

    Resuelvo conflicto merge en web/urls.py

---
+++ ferremas-master/web/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns =[
+    path('', views. home, name='home'),
+
+    path('contacto/', views. contacto, name='contacto'),  
+    path('catalogo/', views. catalogo, name='catalogo'),
+    path('login/', views. login, name='login'),
+    path('registro/', views. registro, name='registro'),
+    path('carrito/', views. carrito, name='carrito'),
+    path('producto_del/<str:pk>', views.producto_del, name='producto_del'),
+    path('agregar_producto', views.agregar_producto, name='agregar_producto'),
+    path('vendedor/', views. vendedor, name='vendedor'),
+    path('contador/', views. contador, name='contador'),
+    path('administrador/', views. administrador, name='administrador'),
+    path('bodeguero/', views. bodeguero, name='bodeguero'),
+]
